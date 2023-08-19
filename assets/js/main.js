e0050e6a9b33983bfd0308918fb30cad9bb518b3
Al Azad Heera <workheera@gmail.com>
2023-08-19 18:31:12 +0600

    template ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
-console.log("Connected!")
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+});
+
+const yearSpan = document.getElementById('year');
+yearSpan.innerHTML = new Date().getFullYear();
